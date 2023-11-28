144d6f37a9da07af6b503e6e233d83e3af6e71cf
vuhoangquoc <hoangvu280701@gmail.com>
2023-11-28 10:26:34 +0700

    create userschema, register (services)

--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,7 +1,8 @@
-import { MongoClient } from 'mongodb'
+import { Collection, Db, MongoClient } from 'mongodb'
 
 // npm i dotenv và import
 import { config } from 'dotenv'
+import User from '~/models/schemas/User.schema'
 config()
 // end
 
@@ -9,18 +10,22 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
   async connect() {
     try {
-      // Send a ping to confirm a successful connection
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
+    } catch (error) {
+      console.log('Error: ', error)
+      throw error
     }
+  }
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
   }
 }
 const databaseService = new DatabaseService()
