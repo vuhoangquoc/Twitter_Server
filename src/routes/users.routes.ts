892e88ef199eee6478eade00bc7d19c117b38385
vuhoangquoc <hoangvu280701@gmail.com>
2023-11-30 17:09:24 +0700

    validate register using checkSchema

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 import { loginController, registerController } from '~/controllers/users.controllers'
-import { loginValidator } from '~/middlewares/users.middlewares'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
 
 const usersRouter = Router()
 
 usersRouter.post('/login', loginValidator, loginController) // cái gì liên quan kết quả trả về, xử lý logic thì đưa vào controller
-usersRouter.post('/register', registerController)
+/**
+ * Description. Register a new user
+ * Path: /register
+ * Method: POST
+ * Body: {name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO8601}
+ */
+usersRouter.post('/register', registerValidator, registerController)
 
 export default usersRouter
