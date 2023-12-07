41523a961c71d0d50053ae1af74495ac24c5878f
vuhoangquoc <hoangvu280701@gmail.com>
2023-12-07 17:10:54 +0700

    Access token middleware logout

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,7 @@
 import { config } from 'dotenv'
 import jwt, { SignOptions } from 'jsonwebtoken'
+import { reject } from 'lodash'
+import { resolve } from 'path'
 config()
 
 export const signToken = ({
@@ -20,6 +22,24 @@ export const signToken = ({
         throw reject(error)
       }
       resolve(token as string)
+    })
+  })
+}
+
+export const verifyToken = ({
+  token,
+  secretOrPublicKey = process.env.JWT_SECRET as string
+}: {
+  token: string
+  secretOrPublicKey?: string
+}) => {
+  return new Promise<jwt.JwtPayload>((resolve, reject) => {
+    jwt.verify(token, secretOrPublicKey, (error, decoded) => {
+      if (error) {
+        console.log(error)
+        throw reject(error)
+      }
+      resolve(decoded as jwt.JwtPayload)
     })
   })
 }
