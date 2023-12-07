e88491c902eae3319f238d532a640f6438345ac3
vuhoangquoc <hoangvu280701@gmail.com>
2023-12-07 14:36:01 +0700

    create collection refresh_tokens & save refresh token to DB

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,4 +1,6 @@
 import { createHash } from 'crypto'
+import { config } from 'dotenv'
+config()
 
 export function sha256(content: string) {
   return createHash('sha3-256').update(content).digest('hex')
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,4 +1,6 @@
+import { config } from 'dotenv'
 import jwt, { SignOptions } from 'jsonwebtoken'
+config()
 
 export const signToken = ({
   payload,
