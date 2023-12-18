a9a1c43772faa426471956e3f70f0829febc8bdc
vuhoangquoc <hoangvu280701@gmail.com>
2023-12-19 00:39:35 +0700

    filterMiddleware: Filter fields that do not want to be sent to the DB

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -15,6 +15,7 @@ import { USERS_MESSAGES } from '~/constants/messages'
 import databaseService from '~/services/database.services'
 import HTTP_STATUS from '~/constants/httpStatus'
 import { UserVerifyStatus } from '~/constants/enum'
+import { pick } from 'lodash'
 
 export const loginController = async (req: Request, res: Response) => {
   // user từ usersmiddleware
@@ -133,6 +134,17 @@ export const updateMeController = async (
 ) => {
   const { user_id } = req.decoded_authorization as TokenPayload
   const { body } = req // lấy body để biết cập nhật field nào
+  // const body = pick(req.body, [
+  //   'name',
+  //   'date_of_birth',
+  //   'bio',
+  //   'location',
+  //   'website',
+  //   'username',
+  //   'avatar',
+  //   'cover_photo'
+  // ])
+  console.log('🚀 ~ file: users.controllers.ts:147 ~ body:', body)
   const user = await usersService.updateMe(user_id, body)
   return res.json({
     message: USERS_MESSAGES.UPDATE_ME_SUCCESS,
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,6 +11,7 @@ import {
   updateMeController,
   verifyForgotPasswordController
 } from '~/controllers/users.controllers'
+import { filterMiddleware } from '~/middlewares/common.middlewares'
 import {
   accessTokenValidator,
   emailVerifyTokenValidator,
@@ -23,6 +24,7 @@ import {
   verifiedUserValidator,
   verifyForgotPasswordTokenValidator
 } from '~/middlewares/users.middlewares'
+import { UpdateMeReqBody } from '~/models/requests/users.requests'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
@@ -117,6 +119,16 @@ usersRouter.patch(
   accessTokenValidator,
   verifiedUserValidator,
   updateMeValidator,
+  filterMiddleware<UpdateMeReqBody>([
+    'name',
+    'date_of_birth',
+    'bio',
+    'location',
+    'website',
+    'username',
+    'avatar',
+    'cover_photo'
+  ]),
   wrapRequestHandler(updateMeController)
 )
 
