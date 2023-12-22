32b8dd7f134a9b6c0142788a72ee1edac105999b
vuhoangquoc <hoangvu280701@gmail.com>
2023-12-22 23:40:14 +0700

    upload file using formidable & init upload folder

---
+++ src/routes/medias.routers.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { uploadSingleImage } from '~/controllers/medias.controllers'
+const mediasRouter = Router()
+
+mediasRouter.post('/upload-image', uploadSingleImage)
+
+export default mediasRouter
