1d9658f0cfb7aa51cf4ae072accc1897d10fc29e
vuhoangquoc <hoangvu280701@gmail.com>
2023-12-26 22:30:09 +0700

    filter upload image

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,24 +1,15 @@
 import { NextFunction, Request, Response } from 'express'
 import formidable from 'formidable'
+import { result } from 'lodash'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/files'
 
 // console.log(__dirname)
 // console.log(path.resolve('uploads'))
 
 export const uploadSingleImage = async (req: Request, res: Response, next: NextFunction) => {
-  // const formidable = (await import("formidable")).default -- fix ES Module CommonJS khi dùng formidable v2
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFieldsSize: 300 * 1024 // 300kb
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: 'Upload image success'
-    })
+  const data = await handleUploadSingleImage(req)
+  return res.json({
+    result: data
   })
 }
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,5 +1,8 @@
 import fs from 'fs'
-import path from 'path'
+import path, { resolve } from 'path'
+import formidable from 'formidable'
+import { Request } from 'express'
+import { reject } from 'lodash'
 
 export const initFolder = () => {
   const uploadFolderPath = path.resolve('uploads')
@@ -8,4 +11,34 @@ export const initFolder = () => {
       recursive: true // Mục đích là tạo folder nested
     })
   }
+}
+
+export const handleUploadSingleImage = async (req: Request) => {
+  // const formidable = (await import("formidable")).default -- fix ES Module CommonJS khi dùng formidable v2
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFieldsSize: 300 * 1024, // 300kb
+    filter: function ({ name, originalFilename, mimetype }) {
+      console.log('🚀 ~ file: files.ts:31 ~ handleUploadSingleImage ~ mimetype:', mimetype)
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
 }
