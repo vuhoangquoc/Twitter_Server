616ab4ff61e7e5a461d6b3227e3ab635cb2c6f2b
vuhoangquoc <hoangvu280701@gmail.com>
2024-01-03 23:40:43 +0700

    upload video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
-import { UPLOAD_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 
@@ -15,10 +15,27 @@ export const uploadImageController = async (req: Request, res: Response, next: N
   })
 }
 
+export const uploadVideoController = async (req: Request, res: Response, next: NextFunction) => {
+  const url = await mediasService.uploadVideo(req)
+  return res.json({
+    message: USERS_MESSAGES.UPLOAD_SUCCESS,
+    result: url
+  })
+}
+
 export const serveImageController = async (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.params
   console.log('🚀 ~ file: medias.controllers.ts:18 ~ serveImageController ~ name:', name)
-  return res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+  return res.sendFile(path.resolve(UPLOAD_IMAGE_DIR, name), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not found')
+    }
+  })
+}
+
+export const serveVideoController = async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
     if (err) {
       res.status((err as any).status).send('Not found')
     }
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -2,21 +2,22 @@ import fs from 'fs'
 import path from 'path'
 import formidable, { File } from 'formidable'
 import { Request } from 'express'
-import { UPLOAD_TEMP_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 
 export const initFolder = () => {
-  const uploadFolderPath = UPLOAD_TEMP_DIR
-  if (!fs.existsSync(uploadFolderPath)) {
-    fs.mkdirSync(uploadFolderPath, {
-      recursive: true // Mục đích là tạo folder nested
-    })
-  }
+  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR].forEach((dir) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true // Mục đích là tạo folder nested
+      })
+    }
+  })
 }
 
 export const handleUploadImage = async (req: Request) => {
   // const formidable = (await import("formidable")).default -- fix ES Module CommonJS khi dùng formidable v2
   const form = formidable({
-    uploadDir: UPLOAD_TEMP_DIR,
+    uploadDir: UPLOAD_IMAGE_TEMP_DIR,
     maxFiles: 4,
     keepExtensions: true,
     maxFileSize: 300 * 1024, // 300kb
@@ -44,8 +45,50 @@ export const handleUploadImage = async (req: Request) => {
   })
 }
 
+export const handleUploadVideo = async (req: Request) => {
+  // const formidable = (await import("formidable")).default -- fix ES Module CommonJS khi dùng formidable v2
+  const form = formidable({
+    uploadDir: UPLOAD_VIDEO_DIR,
+    maxFiles: 1,
+    // keepExtensions: true,
+    maxFileSize: 50 * 1024 * 1024, // 50MB
+    filter: function ({ name, originalFilename, mimetype }) {
+      // console.log('🚀 ~ file: files.ts:31 ~ handleUploadSingleImage ~ mimetype:', mimetype)
+      // const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      // if (!valid) {
+      //   form.emit('error' as any, new Error('File type is not valid') as any)
+      // }
+      // return valid
+      return true
+    }
+  })
+  return new Promise<File[]>((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.video)) {
+        return reject(new Error('File is empty'))
+      }
+      const videos = files.video as File[]
+      videos.forEach((video) => {
+        const ext = getExtension(video.originalFilename as string)
+        fs.renameSync(video.filepath, video.filepath + '.' + ext)
+        video.newFilename = video.newFilename + '.' + ext
+      })
+      resolve(files.video as File[])
+    })
+  })
+}
+
 export const getNameFromFullname = (fullname: string) => {
   const nameArr = fullname.split('.')
   nameArr.pop()
   return nameArr.join('')
+}
+
+export const getExtension = (fullname: string) => {
+  const namearr = fullname.split('.')
+  return namearr[namearr.length - 1]
 }
