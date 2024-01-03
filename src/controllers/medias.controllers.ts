4bf9b3d9b4523777490b1d0278a3020b97eef883
vuhoangquoc <hoangvu280701@gmail.com>
2024-01-03 20:36:30 +0700

    serving static file (c2)

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
+import path from 'path'
+import { UPLOAD_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
 
@@ -10,5 +12,15 @@ export const uploadSingleImage = async (req: Request, res: Response, next: NextF
   return res.json({
     message: USERS_MESSAGES.UPLOAD_SUCCESS,
     result: url
+  })
+}
+
+export const serveImageController = async (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.params
+  console.log('🚀 ~ file: medias.controllers.ts:18 ~ serveImageController ~ name:', name)
+  return res.sendFile(path.resolve(UPLOAD_DIR, name), (err) => {
+    if (err) {
+      res.status((err as any).status).send('Not found')
+    }
   })
 }
